d3eba21e2c6112ae62f02ab9b866e099dc00f756
Brian Lynch <bglynch17@gmail.com>
2018-10-18 10:08:37 +0000

    user now creates project name, make git commit removed

--- scripts/a-up-and-running.sh
+++ scripts/a-up-and-running.sh
@@ -3,17 +3,17 @@
 sudo pip3 install django==2.0
 
 # create django project - same name as to name of the workspace
-django-admin startproject $C9_PROJECT .
+django-admin startproject $1 .
 
 # updated tohe bash_aliases to type 'run' to run the application
 echo -e \alias run=\"python3 ~/workspace/manage.py runserver '\u0024'IP:'\u0024'C9_PORT\" >> ~/.bash_aliases
 
-# add allowed host in settings.py
-sed -i "/ALLOWED_HOSTS/s/\[\]/\['$C9_HOSTNAME'\]/g" ./$C9_PROJECT/settings.py  
+echo -e \alias PROJECTNAME=\"$1\" >> ~/.bash_aliases
 
-# start github and do initial commit
-git init
-git status
+
+
+# add allowed host in settings.py
+sed -i "/ALLOWED_HOSTS/s/\[\]/\['$C9_HOSTNAME'\]/g" ./$1/settings.py  
+
+# add database to git ignore
 echo db.sqlite3 > .gitignore
-git add .
-git commit -m "initial commit"
